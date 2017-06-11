299047b76408f51bf50b9ae318379d3ef330913a
Shrey Desai <shreydesai@me.com>
2017-06-11 12:38:52 -0700

    redesign of about page

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 
     /*
     // partners (WIP)
-    
+
     var partners = $('#partners .ui.image');
 
     for (var i = 0; i < partners.length; i++) {
@@ -25,4 +25,73 @@
     }
     */
 
+    // credits: https://stackoverflow.com/questions/33065051/typed-js-execution-when-scrolled
+    function isViewable(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    var seenOverview = false;
+    var seenTickets = false;
+    var seenVenue = false;
+    var seenPartners = false;
+
+    $(window).scroll(function() {
+        if (isViewable($('#overview-header')) && !seenOverview) {
+            seenOverview = true;
+            $('#overview h1').typed({
+                strings: ['Overview'],
+                typeSpeed: 150
+            });
+            $('#op1').hide().fadeIn(2000);
+            $('#op2').hide().fadeIn(2800);
+        }
+
+        if (isViewable($('#tickets-header')) && !seenTickets) {
+            seenTickets = true;
+            $('#tickets h1').typed({
+                strings: ['Tickets'],
+                typeSpeed: 150
+            });
+
+            // animate tickets show and party
+            $("#tickets-show").css({
+                position: 'relative',
+                opacity: 0,
+                right: "+=100"
+            });
+            $("#tickets-party").css({
+                position: 'relative',
+                opacity: 0,
+                left: "+=100"
+            });
+
+            // start animation
+            $("#tickets-show").animate({
+                right: 0, opacity: 1
+            }, 1000);
+            $("#tickets-party").animate({
+                left: 0, opacity: 1
+            }, 1000);
+        }
+
+        if (isViewable($('#venue-header')) && !seenVenue) {
+            seenVenue = true;
+            $('#venue h1').typed({
+                strings: ['Venue'],
+                typeSpeed: 150
+            });
+        }
+
+        if (isViewable($('#partners-header')) && !seenPartners) {
+            seenPartners = true;
+            $('#partners h1').typed({
+                strings: ['Partners'],
+                typeSpeed: 150
+            });
+        }
+    });
 })(jQuery);
