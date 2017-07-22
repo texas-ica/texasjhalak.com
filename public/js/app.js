0dab047d3aa0838982357bebec5f59a7e09984ea
Shrey Desai <shreydesai@me.com>
2017-07-22 14:44:55 -0700

    added meta tags, optimized images for mobile devices

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,33 @@
-//$('#getinvolved').dropdown();
+// navigation
 
+var path = window.location.pathname;
+
+function resetActive() {
+    var navs = $('#menu-nav').children();
+    for (var i = 0; i < navs.length; i++) {
+        $(navs[i]).removeClass('active');
+    }
+}
+
+function makeActive(path) {
+    resetActive();
+
+    if (path == '/about') {
+        $('#about-nav').addClass('active');
+    } else if (path == '/show') {
+        $('#show-nav').addClass('active');
+    } else if (path == '/afterparty') {
+        $('#afterparty-nav').addClass('active');
+    } else if (path == '/getinvolved') {
+        $('#getinvolved-nav').addClass('active');
+    } else if (path == '/sponsors') {
+        $('#sponsors').addClass('active');
+    }
+}
+
+makeActive(path);
+
+// clock
 var currentDate = new Date();
 var futureDate  = new Date(2017, 11, 18);
 var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
