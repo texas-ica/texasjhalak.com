cb1505df78e303f9a20b60b87a57fb85357ed76c
Shrey Desai <shreydesai@me.com>
2017-07-25 19:58:44 -0700

    rolling back slack integration

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 var express = require('express');
-var dateTime = require('node-datetime');
-var RtmClient = require('@slack/client').RtmClient;
 
-// Express server configuration
 var app = express();
 
-app.set('trust proxy');
 app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
 
-// Slack configuration
-var token = process.env.SLACK_BOT_TOKEN || '';
-var channel = 'C6EGUFEEA';
-var rtm = new RtmClient(token);
-
-// Date/time configuration
-var dt = dateTime.create()
-
-function logEvent(req) {
-    var ip = req.headers['x-real-ip'];
-    var timestamp = dt.format('m/d H:M');
-    var message = 'GET / - ' + ip + ' - ' + timestamp;
-
-    rtm.sendMessage(message, channel, function(err) {
-        if (err) console.log(err);
-    });
-}
-
 app.get('/', function(req, res) {
-    try {
-        logEvent(req);
-    } catch (e) {
-        console.log(e);
-    }
     res.render('pages/index');
 });
 
@@ -59,10 +32,4 @@ app.get('/sponsors', function(req, res) {
 
 app.listen(app.get('port'), function() {
     console.log('Server started on 127.0.0.1');
-    try {
-        console.log('Slack client started');
-        rtm.start();
-    } catch (e) {
-        console.log('ERROR: Slack client failed to start');
-    }
 });
