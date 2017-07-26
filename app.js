812579f0847f23bd54e6ee322a966e66fdfe1979
Shrey Desai <shreydesai@me.com>
2017-07-25 22:44:16 -0700

    added slack integration, aws setup scripts/config

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 var express = require('express');
+var dateTime = require('node-datetime');
+var RtmClient = require('@slack/client').RtmClient;
 
+// Express server configuration
 var app = express();
 
+app.set('trust proxy');
 app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
 
+// Slack configuration
+var token = process.env.SLACK_BOT_TOKEN || '';
+var channel = 'C6EGUFEEA';
+var rtm = new RtmClient(token);
+
+// Date/time configuration
+var dt = dateTime.create()
+
+function logEvent(req) {
+    var ip = req.headers['x-forwarded-for'];
+    var timestamp = dt.format('m/d H:M');
+
+    if (ip) {
+        var message = 'GET / - ' + ip + ' - ' + timestamp;
+        rtm.sendMessage(message, channel, function(err) {
+            if (err) console.log(err);
+        });
+    }
+}
+
 app.get('/', function(req, res) {
+    logEvent(req);
     res.render('pages/index');
 });
 
@@ -32,4 +57,10 @@ app.get('/sponsors', function(req, res) {
 
 app.listen(app.get('port'), function() {
     console.log('Server started on 127.0.0.1');
+    try {
+        console.log('Slack client started');
+        rtm.start();
+    } catch (e) {
+        console.log('ERROR: Slack client failed to start');
+    }
 });
--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,28 +1,32 @@
 # Remove old repo
-echo "(1/7) Removing old repository"
+echo "(1/8) Removing old repository"
 rm -rf texasjhalak.com
 
 # Clone new repo
-echo "(2/7) Cloning new repository"
+echo "(2/8) Cloning new repository"
 git clone https://github.com/texas-ica/texasjhalak.com &> /dev/null
 
 # Stop NGINX
-echo "(3/7) Shutting down NGINX"
+echo "(3/8) Shutting down NGINX"
 sudo service nginx stop &> /dev/null
 
 # Stop Node processes
-echo "(4/7) Stopping Node.js"
+echo "(4/8) Stopping Node.js"
 sudo killall node &> /dev/null
 
 # Set up Node server
-echo "(5/7) Setting up Node.js server"
+echo "(5/8) Setting up Node.js server"
 cd texasjhalak.com
 npm install &> /dev/null
 
 # Start NGINX server
-echo "(6/7) Starting NGINX server"
+echo "(6/8) Starting NGINX server"
 sudo service nginx start &> /dev/null
 
+# Set environment variables
+echo "(7/8) Setting environment variables"
+# TODO: fill with environment variables
+
 # Start server
-echo "(7/7) Starting Node.js server"
+echo "(8/8) Starting Node.js server"
 sudo nohup node app.js &> /dev/null
